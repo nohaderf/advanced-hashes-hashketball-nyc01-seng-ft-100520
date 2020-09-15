1b3751d1d8afb8df421e9b2256c8871f695679b8
Freda Hon <freda.hon@gmail.com>
2020-09-15 20:26:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,9 +134,8 @@ def num_points_scored(player_name)
   game_hash.each do |team, team_values|
     team_values[:players].each do |player_info|
       if player_info[:player_name] == player_name
-                  binding.pry
         return player_info[:score]
-        
+ binding.pry       
       end
     end
   end

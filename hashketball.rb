5afb11d843b6b8cf6b949db9e4980268d4141bb8
Freda Hon <freda.hon@gmail.com>
2020-09-15 20:51:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,7 +185,7 @@ def player_numbers(team_name)
   if game_hash[:home][:team_name] == team_name
     jersey_numbers << game_hash[:home][:players][:number]
   elsif game_hash[:away][:team_name] == team_name
-    jersey_numbers << game_hash[:away][:players][:number]  =
+    jersey_numbers << game_hash[:away][:players][:number]
   end
   jersey_numbers
 end
